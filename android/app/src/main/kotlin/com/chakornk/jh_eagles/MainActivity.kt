e627ef2df8298b42e639c165e5139122206156a0
ChakornK <chakornk2007@gmail.com>
2024-04-16 13:09:23 -0700

    withtimeout was not the right function 🗿

--- android/app/src/main/kotlin/com/chakornk/jh_eagles/MainActivity.kt
+++ android/app/src/main/kotlin/com/chakornk/jh_eagles/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import io.flutter.embedding.android.FlutterActivity
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 
@@ -20,28 +21,16 @@ class MainActivity : FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    // The splash screen goes edge to edge, so for a smooth transition to our app, also
-    // want to draw edge to edge.
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-
-    // The content view needs to be set before calling setOnExitAnimationListener
-    // to ensure that the SplashScreenView is attached to the right view root.
+    // attach splashscreen
     val rootLayout: FrameLayout = findViewById(android.R.id.content)
     View.inflate(this, R.layout.main_activity, rootLayout)
 
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { view, windowInsets ->
-      val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-      view.setPadding(insets.left, insets.top, insets.right, insets.bottom)
-      windowInsets.inset(insets)
-    }
-
     runBlocking {
-      withTimeout(2000) {
-        if (flutterUIReady) {
-          hideSplashScreenAnimation()
-        } else {
-          animationDone = true
-        }
+      delay(2000)
+      if (flutterUIReady) {
+        hideSplashScreenAnimation()
+      } else {
+        animationDone = true
       }
     }
   }
@@ -62,10 +51,10 @@ class MainActivity : FlutterActivity() {
    */
   private fun hideSplashScreenAnimation(){
     val splashView: ConstraintLayout = findViewById(R.id.container)
-     splashView
-       .animate()
-       .alpha(0.0f)
-       .setDuration(SPLASH_SCREEN_TRANSITION_DURATION)
+    splashView
+      .animate()
+      .alpha(0.0f)
+      .setDuration(SPLASH_SCREEN_TRANSITION_DURATION)
   }
 
   private companion object {
