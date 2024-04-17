5bf93067fd5e6c9a0ffe1a0ad95684382165609f
ChakornK <chakornk2007@gmail.com>
2024-04-17 09:07:50 -0700

    basic calendar page layout

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,17 +1,16 @@
 import 'dart:convert';
+import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:jiffy/jiffy.dart';
+import 'package:intl/intl.dart';
 
+int lastEventDay = 0;
 int lastEventWeek = 0;
 int lastEventMonth = 0;
-
-int findWeekOfYear(day, year) {
-  // 1 is monday, 7 is sunday
-  int weekdayOfJan1 = DateTime(year, 1, 1).weekday;
-  int weekNumber = (day + weekdayOfJan1 - 1) ~/ 7;
-  return weekNumber;
-}
 
 class CalendarEvent {
   int sid;
@@ -81,6 +80,8 @@ class _CalendarPageState extends State<CalendarPage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints viewportConstraints) {
       return SingleChildScrollView(
         child: ConstrainedBox(
@@ -89,65 +90,180 @@ class _CalendarPageState extends State<CalendarPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Center(
-                  child: Padding(
-                padding: const EdgeInsets.all(16.0),
                 child: Column(
                   children: [
                     FutureBuilder<List<CalendarEvent>>(
-                        future: futureData,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                            print("${snapshot.data!.length}");
-                            return Column(
-                                children: snapshot.data!.map((e) {
-                              List<Widget> widgetList = [];
+                      future: futureData,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                          print("${snapshot.data!.length}");
+                          return Column(
+                              children: snapshot.data!.map((e) {
+                            List<Widget> headerWidgetList = [];
+                            Widget? dateIndicator;
 
-                              List<String> date = e.date.toString().split("-");
-                              int day = int.parse(date[2]);
-                              int month = int.parse(date[1]);
-                              int year = int.parse(date[0]);
+                            DateTime date = DateTime.parse(e.date);
 
-                              if (month != lastEventMonth) {
-                                widgetList.add(Text("${date[1]}/${date[2]}"));
-                              }
+                            int currentWeek = Jiffy.parseFromDateTime(DateTime.parse(e.date)).weekOfYear;
 
-                              int currentWeek = findWeekOfYear(day, year);
-                              if (currentWeek != lastEventWeek) {
-                                lastEventWeek = currentWeek;
-                                widgetList.add(Text("Week $currentWeek"));
-                              }
-                              return Column(
-                                children: widgetList,
-                              );
-                            }).toList());
-                          } else if (snapshot.hasData && snapshot.data!.isEmpty && !isLoading) {
-                            return Column(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Text('${snapshot.error ?? "Failed to load data"}'),
-                                SizedBox(height: 10),
-                                FilledButton.tonalIcon(
-                                  onPressed: () {
-                                    isLoading = true;
-                                    futureData = fetchData();
-                                    setState(() {});
-                                  },
-                                  icon: Icon(
-                                    Icons.refresh_rounded,
-                                    size: 20,
+                            if (date.month != lastEventMonth) {
+                              lastEventMonth = date.month;
+                              headerWidgetList.add(
+                                  // Card(
+                                  //   color: theme.buttonTheme.colorScheme?.primaryContainer,
+                                  //   child: Padding(
+                                  //     padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+                                  //     child: Text(
+                                  //       DateFormat("MMMM yyyy").format(date),
+                                  //       style: TextStyle(
+                                  //         color: theme.buttonTheme.colorScheme?.onPrimaryContainer,
+                                  //         fontSize: theme.textTheme.headlineSmall?.fontSize,
+                                  //       ),
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                  SizedBox(
+                                child: DecoratedBox(
+                                  decoration: BoxDecoration(
+                                    color: theme.buttonTheme.colorScheme?.primaryContainer,
                                   ),
-                                  label: Text("Retry"),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(24.0),
+                                    child: Text(
+                                      DateFormat("MMMM yyyy").format(date),
+                                      style: TextStyle(
+                                        color: theme.buttonTheme.colorScheme?.onPrimaryContainer,
+                                        fontSize: theme.textTheme.headlineSmall?.fontSize,
+                                      ),
+                                    ),
+                                  ),
                                 ),
-                              ],
-                            );
-                          }
+                              ));
+                            }
 
-                          // By default, show a loading spinner.
-                          return const CircularProgressIndicator();
-                        })
+                            // add week indicator
+                            if (currentWeek != lastEventWeek) {
+                              lastEventWeek = currentWeek;
+
+                              // find the start and end date of the week
+                              DateFormat formatter = DateFormat("MMM d");
+                              DateTime startOfWeek = date.subtract(Duration(days: date.weekday - 7));
+                              DateTime endOfWeek = startOfWeek.add(Duration(days: 6));
+
+                              headerWidgetList.add(
+                                Padding(
+                                  padding: EdgeInsets.only(top: 16, bottom: 12, left: 78),
+                                  child: Text(
+                                    "${formatter.format(startOfWeek)} - ${formatter.format(endOfWeek)}",
+                                  ),
+                                ),
+                              );
+                            } else if (headerWidgetList.length == 1) {
+                              headerWidgetList.add(SizedBox(height: 24));
+                            }
+
+                            if (date.day != lastEventDay) {
+                              lastEventDay = date.day;
+                              bool isToday = Jiffy.parseFromDateTime(date).dayOfYear == Jiffy.parseFromDateTime(DateTime.now()).dayOfYear &&
+                                  Jiffy.parseFromDateTime(date).year == Jiffy.parseFromDateTime(DateTime.now()).year;
+                              dateIndicator = SizedBox(
+                                width: 42,
+                                height: 42,
+                                child: DecoratedBox(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(69)),
+                                    color: isToday ? theme.buttonTheme.colorScheme?.primary : Colors.transparent,
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        height: 13,
+                                        child: Text(
+                                          DateFormat("EEE").format(date),
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: isToday ? theme.buttonTheme.colorScheme?.onPrimary : theme.textTheme.bodySmall?.color,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                        child: Text(
+                                          "${date.day}",
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: isToday ? theme.buttonTheme.colorScheme?.onPrimary : theme.textTheme.bodySmall?.color,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }
+
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  (dateIndicator != null ? SizedBox(height: 24) : Container()),
+                                  ...headerWidgetList,
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                                    child: Row(
+                                      children: [
+                                        (dateIndicator ?? SizedBox(width: 42)),
+                                        SizedBox(width: 8),
+                                        Expanded(
+                                          child: Card.filled(
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                                children: [Text(e.name)],
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }).toList());
+                        } else if (snapshot.hasData && snapshot.data!.isEmpty && !isLoading) {
+                          return Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text('${snapshot.error ?? "Failed to load data"}'),
+                              SizedBox(height: 10),
+                              FilledButton.tonalIcon(
+                                onPressed: () {
+                                  isLoading = true;
+                                  futureData = fetchData();
+                                  setState(() {});
+                                },
+                                icon: Icon(
+                                  Icons.refresh_rounded,
+                                  size: 20,
+                                ),
+                                label: Text("Retry"),
+                              ),
+                            ],
+                          );
+                        }
+
+                        // By default, show a loading spinner.
+                        return const CircularProgressIndicator();
+                      },
+                    )
                   ],
                 ),
-              ))
+              ),
             ],
           ),
         ),
